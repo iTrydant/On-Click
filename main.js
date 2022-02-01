0624028a2a0ce38e5f48746f3afb65347dd20e93
Yash Gautam <98219582+iTrydant@users.noreply.github.com>
2022-02-01 10:25:45 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("clicked").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  document.getElementById("clicked").innerHTML = "Thanks for clicking me!";
+}
